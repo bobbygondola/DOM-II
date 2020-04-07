b587f72b906d9f7843d227b9cd50e88dc13a5ea4
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-04-07 19:10:45 -0400

    wip2

--- js/index.js
+++ js/index.js
@@ -1,49 +1,68 @@
 // GET FUNCTION
 const get = (selector) => {
-    return document.querySelector(selector);
-  };
+  return document.querySelector(selector);
+};
 
-  const getAll = (selector) => {
-    return document.querySelectorAll(selector);
-  };
+const getAll = (selector) => {
+  return document.querySelectorAll(selector);
+};
 
 // VARIABLES
-const logoo = get(".logo-heading")
-const navv = get(".nav")
-const first = get(".intro")
-const lets = get(".text-content")
-const wideimg = get('#wide')
+const logoo = get(".logo-heading");
+const navv = get(".nav");
+const first = get(".intro");
+const lets = get(".text-content");
+const wideimg = get("#wide");
+const body = get("body");
 
 ////////////All Selector
-const buttons = getAll('.btn')
-
+const buttons = getAll(".btn");
 
 ////////////Button Function
-function red(event){
-    event.target.style.backgroundColor = "red";
-};
-for(let i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener("click", red)
-};
+function red(event) {
+  event.target.style.backgroundColor = "red";
+}
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("dblclick", red);
+}
 //////////Button Text Color
-function black(event){
-    event.target.style.color = "black";
-};
-for(let i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener("click", black)
-};
+function black(event) {
+  event.target.style.color = "black";
+}
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("dblclick", black);
+}
 
 ///////////Logo Hover
 
-function logoHover(event){
-    event.target.style.color = "blue";
-    
-};
-logoo.addEventListener("mouseover",logoHover);
+function logoHover(event) {
+  event.target.style.color = "blue";
+}
+logoo.addEventListener("mouseover", logoHover);
 
-function logoLeave(event){
-    event.target.style.color = "black";
-};
-logoo.addEventListener("mouseleave", logoLeave)
+function logoLeave(event) {
+  event.target.style.color = "black";
+}
+logoo.addEventListener("mouseleave", logoLeave);
 
+//////////// Troll Opacity Body
 
+function Opacity(event) {
+  event.target.style.opacity = 1;
+  if (event.key === "ArrowDown") {
+    event.target.style.opacity = 0.5;
+  }
+}
+body.addEventListener("keydown", Opacity);
+
+//////// Load Event
+function loader(event) {
+  event.target.style.display = "none";
+}
+wideimg.addEventListener("load", loader);
+
+////// select event
+function selecter(event) {
+  event.target.style.display = "none";
+}
+navv.addEventListener("select", selecter);
